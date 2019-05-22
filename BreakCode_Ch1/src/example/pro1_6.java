dc406a2c9f0cf0857e0f759caba6c747f316280c
연도현 <dohyun_yeon@tmax.co.kr>
2019-05-23 03:02:38 +0900

    6번

--- BreakCode_Ch1/src/example/pro1_6.java
+++ BreakCode_Ch1/src/example/pro1_6.java
@@ -1,10 +1,38 @@
 package example;
 
-public class pro1_6 {
+import java.util.ArrayList;
 
+public class pro1_6 {
+	public static Boolean samechar (Character arg0, Character arg1) {
+		if (arg0 == arg1) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public static String StrCompress(String arg0) {
+		StringBuffer ss = new StringBuffer();
+		ArrayList<Character> ar_word = new ArrayList<>();
+		for(int i=0;i<arg0.length();i++) {
+			ar_word.add(arg0.charAt(i));
+		}
+		int a=1;
+		for(int i=0;i<arg0.length()-1;i++) {
+			if (samechar(ar_word.get(i),ar_word.get(i+1))){
+				a++;
+			}else {
+				ss.append(ar_word.get(i));
+				ss.append(a);
+				a=1;
+			}
+		}
+		ss.append(ar_word.get(arg0.length()-1));
+		ss.append(a);
+		return ss.toString();
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(StrCompress("aabcccccaaaCccd"));
 	}
 
 }
